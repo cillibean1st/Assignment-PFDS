01b33b667fdf88a298b8e7bcf34d862192f3e8e5
Cillibean <andrew@capsecum.co.uk>
2025-05-14 14:05:53 +0100

    commit 2 - Mid clean

--- Assignment 1.py
+++ Assignment 1.py
@@ -2,7 +2,17 @@
 import numpy as np
 import pandas as pd
 
+
 # read data
-taxi_pool_data = open(r"TaxiRideShare .csv", "r+")
+taxi_pool_df = pd.read_csv('TaxiRideShare .csv')
+taxi_pool_df.head()
+taxi_pool_df.info()
+taxi_pool_df.describe()
 
 
+
+# cleaning
+taxi_pool_df = taxi_pool_df.drop(columns="timestamp")
+taxi_pool_df = taxi_pool_df.drop(columns="datetime")
+
+taxi_pool_df = taxi_pool_df["source"].dropna()
